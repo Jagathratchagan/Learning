0425c59b5e5959a2b1314d3d8b6b8486bc6de729
Jagathratchagan J <jagathratchagan@gmail.com>
2024-07-01 20:17:42 +0530

    .

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class SentenceReverser
+{   
+
+    public static void Main()
+    {
+
+        Console.WriteLine("Please enter your input sting : ");
+
+        // Get input
+        string input = Console.ReadLine();
+
+        // Reverse logic
+        string output = ReverseWords(input);
+
+        Console.WriteLine("Original: " + input);
+        Console.WriteLine("Reversed: " + output);
+    }
+
+    public static string ReverseWords(string sentence)
+    {        
+        string[] words = sentence.Split(' ');
+        
+        Array.Reverse(words);
+        
+        string reversedSentence = string.Join(" ", words);
+
+        return reversedSentence;
+    }
+
+}
+
